a3631f4104e3bc75f8a150bd551835dfcd3c2e54
12xx12 <12xx12100@gmail.com>
2020-05-13 02:39:33 +0200

    added another javadoc

--- src/de/malo/timber/Timber.java
+++ src/de/malo/timber/Timber.java
@@ -20,6 +20,8 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.UnknownDependencyException;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -33,6 +35,8 @@ public class Timber extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
+        // checks if WorldGuard is installed
+        // Todo: make this work without WorldGuard
         WorldGuardPlugin worldGuard = getWorldGuard();
         if (worldGuard == null) {
             getLogger().info("using no WorldGuard");
@@ -45,10 +49,10 @@ public class Timber extends JavaPlugin implements Listener {
         // config stuff
         this.saveDefaultConfig();
         FileConfiguration config = this.getConfig();
+        // Todo: add toggle for logging
         config.addDefault("maxChop", 8);
         config.options().copyDefaults(true);
         saveConfig();
-
     }
 
     @EventHandler
@@ -81,7 +85,7 @@ public class Timber extends JavaPlugin implements Listener {
      * @author Marc
      * @since 2020-05-05
      */
-    private void dropTree(final Location location, final Player player) {
+    private void dropTree(final @NotNull Location location, final Player player) {
         List<Block> checkedBlocks = new LinkedList<>();
 
         Location origin = location.clone();
@@ -93,22 +97,22 @@ public class Timber extends JavaPlugin implements Listener {
         }
         damageItem(player.getInventory().getItemInMainHand(), blocks.size());
         player.updateInventory();
+
+        //Todo: add logger functionality
     }
 
     /**
      * adds connected blocks to the removal list.
-     * checks surroundings for leaves and Logs and adds them
-     * Logs directly if connected to the original block and
-     * for leaves checks if is not part of another tree
+     * checks if the surrounding 18 block at y = ~ and y = ~+1 are a log and adds them
      *
      * @param location      location of the
      * @param origin        block which was already mined
-     * @param checkedBlocks leaves already checked
+     * @param checkedBlocks blocks already checked
      * @return list of blocks to break
      * @author Marc
      * @since 2020-05-05
      */
-    private List<Block> leaveDrop(Location location, final Location origin, List<Block> checkedBlocks) {
+    private @NotNull List<Block> leaveDrop(Location location, final Location origin, List<Block> checkedBlocks) {
         List<Block> breakBlogs = new LinkedList<>(); // list returned with the blocks to break
         final float border = getConfig().getInt("maxChop"); // maximum distance to original broken block
         for (int x = -1; x <= 1; x++) {
@@ -153,7 +157,7 @@ public class Timber extends JavaPlugin implements Listener {
      * @author Marc
      * @since 2020-05-05
      */
-    private boolean isAxe(ItemStack material) {
+    private boolean isAxe(@NotNull ItemStack material) {
         boolean isAxe;
         switch (material.getType()) {
             case WOODEN_AXE:
@@ -178,7 +182,7 @@ public class Timber extends JavaPlugin implements Listener {
      * @author Marc
      * @since 2020-05-05
      */
-    private boolean isLog(Material material) {
+    private boolean isLog(@NotNull Material material) {
         boolean isLog;
         switch (material) {
             case ACACIA_LOG:
@@ -206,7 +210,7 @@ public class Timber extends JavaPlugin implements Listener {
      * @author Marc
      * @since 2020-05-13
      */
-    private int distance(Location pos1, Location pos2) {
+    private int distance(@NotNull Location pos1, @NotNull Location pos2) {
         int dis = (int) round(sqrt(pow(pos1.getBlockX() - pos2.getBlockX(), 2) + pow(pos1.getBlockZ() - pos2.getBlockZ(), 2)));
         dis += round(abs(pos1.getBlockY() - pos2.getBlockY()) / 8.0);
         return dis;
@@ -221,7 +225,7 @@ public class Timber extends JavaPlugin implements Listener {
      * @author Marc
      * @since 2020-05-05
      */
-    private ItemStack damageItem(ItemStack item, int damage) {
+    private @NotNull ItemStack damageItem(@NotNull ItemStack item, int damage) {
         org.bukkit.inventory.meta.Damageable im = (org.bukkit.inventory.meta.Damageable) item.getItemMeta();
         // checks for unbraking and respects it in the damage value
         if (item.containsEnchantment(Enchantment.DURABILITY)) {
