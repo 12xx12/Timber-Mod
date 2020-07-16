f0d41e904a1d3b5043a4969d256f28eaa04e4e23
12xx12 <12xx12100@gmail.com>
2020-07-16 11:25:53 +0200

    added new plugin with maven

---
+++ src/main/java/me/malo/timbermod/timber/Timber.java
@@ -0,0 +1,241 @@
+package me.malo.timbermod.timber;
+
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldguard.LocalPlayer;
+import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.protection.flags.Flags;
+import com.sk89q.worldguard.protection.regions.RegionContainer;
+import com.sk89q.worldguard.protection.regions.RegionQuery;
+import org.bukkit.*;
+import org.bukkit.block.Block;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+import static java.lang.Math.*;
+
+public class Timber extends JavaPlugin implements Listener {
+
+    static boolean withWorldGuard;
+    static CommandToggle toggle;
+    private HashMap<Player, Boolean> playerStatus;
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+
+        // checks if WorldGuard is installed and disables functionality if installed
+        if (Bukkit.getPluginManager().getPlugin("WorldGuard") == null) {
+            getLogger().info("using no WorldGuard");
+            withWorldGuard = false;
+        } else {
+            getLogger().info("using WorldGuard");
+            withWorldGuard = true;
+        }
+
+        // config stuff
+        this.saveDefaultConfig();
+        FileConfiguration config = this.getConfig();
+        config.addDefault("maxChop", 8);
+        config.addDefault("logging", false);
+        config.options().copyDefaults(true);
+        saveConfig();
+
+        // add player tracking
+        playerStatus = new HashMap<>();
+        toggle = new CommandToggle(playerStatus);
+
+        // regestering command
+        this.getCommand("toggletimber").setExecutor(new CommandToggle(playerStatus));
+    }
+
+    @EventHandler
+    private void onBlockBreak(BlockBreakEvent e) {
+        boolean canBuild;
+        // checks if targeted block is buildable
+        if (withWorldGuard) {
+            LocalPlayer localPlayer = com.sk89q.worldguard.bukkit.WorldGuardPlugin.inst().wrapPlayer(e.getPlayer());
+            RegionContainer container = WorldGuard.getInstance().getPlatform().getRegionContainer();
+            RegionQuery query = container.createQuery();
+            com.sk89q.worldedit.util.Location loc = BukkitAdapter.adapt(e.getBlock().getLocation());
+            canBuild = query.testState(loc, localPlayer, Flags.BUILD);
+        } else {
+            // if no WorldGuard is installed set to permanent true
+            canBuild = true;
+        }
+        if (!e.getPlayer().isSneaking() && canBuild)
+            if (e.getPlayer().hasPermission("timber.use"))
+                if (toggle.getTimber(e.getPlayer()) == Boolean.TRUE)
+                    if (isAxe(e.getPlayer().getInventory().getItemInMainHand()))
+                        if (isLog(e.getBlock().getType()))
+                            dropTree(e.getBlock().getLocation(), e.getPlayer());
+
+    }
+
+    /**
+     * Drops the tree at the given location
+     *
+     * @param location location of the block destroyed
+     * @param player   player who chopped the tree
+     * @author Marc
+     * @since 2020-05-05
+     */
+    private void dropTree(final @NotNull Location location, final Player player) {
+        List<Block> checkedBlocks = new LinkedList<>();
+        Location origin = location.clone();
+        Location leaveLocation = location.clone();
+
+        List<Block> blocks = leaveDrop(leaveLocation, origin, checkedBlocks);
+
+        for (Block block : blocks) {
+            block.breakNaturally(player.getInventory().getItemInMainHand());
+        }
+        player.getInventory().setItemInMainHand(damageItem(player.getInventory().getItemInMainHand(), player, blocks.size()));
+        player.updateInventory();
+
+        // logging section
+        if (getConfig().getBoolean("logging")) {
+            getLogger().info("Player " + player.getName() + " broke " + (blocks.size()) +
+                    " blocks at [" + origin.getBlockX() + ", " + origin.getBlockY() + ", " + origin.getBlockZ() + "]");
+        }
+    }
+
+    /**
+     * adds connected blocks to the removal list.
+     * checks if the surrounding 18 block at y = ~ and y = ~+1 are a log and adds them
+     *
+     * @param location      location of the
+     * @param origin        block which was already mined
+     * @param checkedBlocks blocks already checked
+     * @return list of blocks to break
+     * @author Marc
+     * @since 2020-05-05
+     */
+    private @NotNull List<Block> leaveDrop(Location location, final Location origin, List<Block> checkedBlocks) {
+        List<Block> breakBlogs = new LinkedList<>(); // list returned with the blocks to break
+        final float border = getConfig().getInt("maxChop"); // maximum distance to original broken block
+        for (int x = -1; x <= 1; x++) {
+            for (int y = 0; y <= 1; y++) {
+                for (int z = -1; z <= 1; z++) {
+                    Location dest = location.getBlock().getRelative(x, y, z).getLocation();
+                    if (!(checkedBlocks.contains(dest.getBlock()))) {
+                        checkedBlocks.add(dest.clone().getBlock()); // adds to already checked blocks
+                        if (isLog(dest.getBlock().getType()) && distance(location, origin) < border) {
+                            if (!(breakBlogs.contains(dest.getBlock())))
+                                breakBlogs.add(dest.clone().getBlock()); // adds to blocks to break if not already added
+                            breakBlogs.addAll(leaveDrop(dest, origin, checkedBlocks));
+                        }
+                    }
+                }
+            }
+        }
+        return breakBlogs;
+    }
+
+    /**
+     * determines if the given material is an axe
+     *
+     * @param material the material to check
+     * @return whether it is or not
+     * @author Marc
+     * @since 2020-05-05
+     */
+    private boolean isAxe(@NotNull ItemStack material) {
+        boolean isAxe;
+        switch (material.getType()) {
+            case WOODEN_AXE:
+            case STONE_AXE:
+            case IRON_AXE:
+            case GOLDEN_AXE:
+            case DIAMOND_AXE:
+                isAxe = true;
+                break;
+            default:
+                isAxe = false;
+                break;
+        }
+        return isAxe;
+    }
+
+    /**
+     * determine is the given material is a log
+     *
+     * @param material the material to check
+     * @return whether it is or not
+     * @author Marc
+     * @since 2020-05-05
+     */
+    private boolean isLog(@NotNull Material material) {
+        boolean isLog;
+        switch (material) {
+            case ACACIA_LOG:
+            case BIRCH_LOG:
+            case DARK_OAK_LOG:
+            case JUNGLE_LOG:
+            case OAK_LOG:
+            case SPRUCE_LOG:
+            case WARPED_STEM:
+            case CRIMSON_STEM:
+                isLog = true;
+                break;
+            default:
+                isLog = false;
+                break;
+        }
+        return isLog;
+    }
+
+    /**
+     * returns the customised distance
+     * the y - position is less influential to the distance
+     *
+     * @param pos1 starting position of the distance
+     * @param pos2 ending position of the distance
+     * @return the distance between pos1 and pos2
+     * @author Marc
+     * @since 2020-05-13
+     */
+    private int distance(@NotNull Location pos1, @NotNull Location pos2) {
+        int dis = (int) round(sqrt(pow(pos1.getBlockX() - pos2.getBlockX(), 2) + pow(pos1.getBlockZ() - pos2.getBlockZ(), 2)));
+        dis = (int) round(sqrt(pow((pos1.getBlockY() - pos2.getBlockY()) / 8.0, 2) + pow(dis, 2)));
+        return dis;
+    }
+
+    /**
+     * damages the given item by the given value, respecting unbreaking.
+     *
+     * @param item   - item to damage
+     * @param damage - damage to deal to item
+     * @return the damaged item
+     * @author Marc
+     * @since 2020-05-05
+     */
+    private @NotNull ItemStack damageItem(@NotNull ItemStack item, Player player, int damage) {
+        org.bukkit.inventory.meta.Damageable im = (org.bukkit.inventory.meta.Damageable) item.getItemMeta();
+        // checks for unbreaking and respects it in the damage value
+        if (item.containsEnchantment(Enchantment.DURABILITY))
+            damage = (int) round(damage * (1.0 / (item.getEnchantmentLevel(Enchantment.DURABILITY) + 1)));
+
+        for (int i = 0; i <= damage; i++) {
+            im.setDamage(im.getDamage() + 1);
+            item.setItemMeta((org.bukkit.inventory.meta.ItemMeta) im);
+        }
+        if (item.getType().getMaxDurability() - im.getDamage() <= 0) {
+            item.setType(Material.AIR);
+            player.playSound(player.getLocation(), Sound.ENTITY_ITEM_BREAK, 1F, 1F);
+            player.spawnParticle(Particle.ITEM_CRACK, player.getLocation(), 1, 0, 0, 0,
+                    player.getInventory().getItemInMainHand());
+        }
+        return item;
+    }
+}
